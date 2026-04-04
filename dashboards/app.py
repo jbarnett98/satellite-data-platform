b291d1100834cd9d1d1d24b91d2fe94e79788aba
Jack Barnett <barnettjack2929@gmail.com>
2026-04-04 20:08:46 +0100

    A

--- dashboards/app.py
+++ dashboards/app.py
@@ -2,10 +2,9 @@ import streamlit as st
 import pandas as pd
 import sys
 from pathlib import Path
-from config import ENRICHED_DATA_PATH
+from scripts.config import ENRICHED_DATA_PATH
 
 sys.path.append(str(Path(__file__).resolve().parent.parent / "scripts"))
-
 
 st.set_page_config(page_title="Satellite Dashboard", layout="wide")
 
